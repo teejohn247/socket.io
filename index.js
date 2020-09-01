0970e84939de8ffc1ad940ae8dab7e03a70e2293
teejohn247 <teejohn247@gmail.com>
2020-09-01 10:58:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+    // var db = mysql.createConnection({
+    //     host: 'localhost',
+    //     user: 'root',
+    //     database: 'node'
+    // });
+
+    // db.connect(func
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        console.log(data);
+        io.sockets.emit('chat', data);
+        // db.query('INSERT INTO notes (note) VALUES (?)', data)
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
+});
